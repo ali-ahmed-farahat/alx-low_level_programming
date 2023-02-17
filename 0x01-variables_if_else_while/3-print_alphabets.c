55db8c40b88f89ba30739034f8db76fb8c9c818b
alialmongy@gmail.com <alialmongy@gmail.com>
2023-02-17 15:03:56 -0800

    3-print_alphabets.c upper and lower

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+ * main - prints the alphabet
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+char lower[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i;
+
+for (i = 0; i < 26; i++)
+{
+putchar(alp[i]);
+}
+for (i = 0; i < 26; i++)
+{
+putchar(lower[i]);
+}
+printf("\n");
+return (0);
+}
