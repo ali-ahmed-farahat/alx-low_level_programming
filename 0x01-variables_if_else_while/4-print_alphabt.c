5a13671c661a022d5672b17587e46f05c0235a53
alialmongy@gmail.com <alialmongy@gmail.com>
2023-02-17 15:11:00 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints the alphabet
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+char alp[26] = "abdefghijklmnoprstuvwxyz\n";
+int i;
+
+for (i = 0; i < 26; i++)
+{
+putchar(alp[i]);
+}
+return (0);
+}
+
