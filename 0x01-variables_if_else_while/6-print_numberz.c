b7e9292bed8b5a1b16246d776bf3245cfa6abcca
alialmongy@gmail.com <alialmongy@gmail.com>
2023-02-17 15:56:52 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints the digits from 0 to 9
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
