8ed62ecc5ccc730f5678b9bc885f8656ead62593
alialmongy@gmail.com <alialmongy@gmail.com>
2023-02-16 16:35:45 -0800

    puts.c hello world C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ *main - prints "Programming is lie building a multilingual puzzle,followed by a new line"
+ *returns 0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
