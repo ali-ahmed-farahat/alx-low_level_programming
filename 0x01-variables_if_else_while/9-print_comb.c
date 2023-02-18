e282313f4c63faa4da33be89dcc83dc5521711b5
alialmongy@gmail.com <alialmongy@gmail.com>
2023-02-17 16:18:24 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints the digits from 0 to 9
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+if (i != '9')
+{
+    putchar(',');
+    putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
