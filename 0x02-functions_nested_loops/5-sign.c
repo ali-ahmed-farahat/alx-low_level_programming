efbc8a5e4370f49713eeab41cf3fdc826977f2a0
alialmongy@gmail.com <alialmongy@gmail.com>
2023-02-21 17:51:24 -0800

    Sign detector

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * print_sign - detects whether is it +ve, -ve or 0
+ * @n: the number to be checked
+ * Return: 1 for +ve and 0 for zero and -1 for -ve
+ */
+int print_sign(int n)
+{
+if (n < 0)
+{
+printf("-");
+return (-1);
+}
+else if (n == 0)
+{
+printf("0");
+return (0);
+}
+else
+{
+printf("+");
+return (1);
+}
+}
+
