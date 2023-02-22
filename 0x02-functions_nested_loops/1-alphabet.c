bbfec77688c50c8b30282bd6305df8c3555e0746
alialmongy@gmail.com <alialmongy@gmail.com>
2023-02-21 16:49:35 -0800

    printing the alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * main -function that prints alpt
  * print_alphabet - prints the alphbts
  * Return: Always 0 (Success
  */
 
-void print_alphabet(void);
-int main(void)
-{
-print_alphabet();
-return (0);
-}
 void print_alphabet(void)
 {
 char i;
