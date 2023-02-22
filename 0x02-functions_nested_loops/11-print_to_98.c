103bcb2805528e4227bca28e00c0c05eb8c6294f
alialmongy@gmail.com <alialmongy@gmail.com>
2023-02-21 19:39:33 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,9 +8,25 @@
 
 void print_to_98(int n)
 {
-for ( ; n <= 98 ; n++)
+if (n < 98)
+{
+for ( ; n < 98 ; n++)
 {
 printf("%d, ", n);
+}
+printf("98");
+}
+else if (n > 98)
+{
+for ( ; n > 98 ; n--)
+{
+printf("%d, ", n);
+}
+printf("98");
+}
+else if (n == 98)
+{
+	printf("98");
 }
 printf("\n");
 }
