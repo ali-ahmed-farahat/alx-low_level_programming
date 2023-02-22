a94a54207f8202eaa37426fe7e4b2f7747989189
alialmongy@gmail.com <alialmongy@gmail.com>
2023-02-21 19:18:25 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,14 +9,14 @@ void times_table(void)
 int x, y, z, u, d;
 for (x = 0; x <= 9; x++)
 {
-for (y = 0; y <= 0; y++)
+for (y = 0; y <= 9; y++)
 {
 z = x * y;
 if (z > 9)
 {
 u = z % 10;
 d = (z - u) / 10;
-_putchar(64);
+_putchar(44);
 _putchar(32);
 _putchar(d + '0');
 _putchar(u + '0');
@@ -25,7 +25,7 @@ else
 {
 if (y != 0)
 {
-_putchar(64);
+_putchar(44);
 _putchar(32);
 _putchar(32);
 }
