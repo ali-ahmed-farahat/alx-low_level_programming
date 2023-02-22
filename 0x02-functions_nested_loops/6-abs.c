17d8806e426aac832d17ab32ed9f0d33c6a1a614
alialmongy@gmail.com <alialmongy@gmail.com>
2023-02-21 18:08:35 -0800

    absolute of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - shows the abs of a number
+ * @c: the number the be absoluted
+ * Return: abs (c)
+ */
+
+int _abs(int c)
+{
+int c;
+return abs(c);
+}
