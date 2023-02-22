190b378bafcdec0c7d9e7bd17fbba2d7ff6a1024
alialmongy@gmail.com <alialmongy@gmail.com>
2023-02-21 18:42:13 -0800

    Gets the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -15,16 +15,6 @@ if (last < 0)
 {
 last = last * -1; 
 }
-_putchar(char(last) + '0');
-return (last);
-}int print_last_digit(int n)
-{
-int last;
-last = n % 10;
-if (last < 0)
-{
-last = last * -1; 
-}
 _putchar(last + '0');
 return (last);
 }
