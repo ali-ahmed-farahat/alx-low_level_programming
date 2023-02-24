c47b606868b5c5d260eabf2fe868814bb9e3279c
alialmongy@gmail.com <alialmongy@gmail.com>
2023-02-23 16:16:44 -0800

    Showing upper and lower characters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * _isupper - shows whether a character is upper or lower
+ * @c: is the chracter which will be tested
+ * Return: 1 if upper, 0 if otherwise
+ */
+int _isupper(int c)
+{
+return (isupper(c));
+}
