1e7af808697495b5fb380f7807c0feea691cfa06
alialmongy@gmail.com <alialmongy@gmail.com>
2023-02-28 10:41:05 -0800

    String to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,12 +4,36 @@
 #include "main.h"
 
 /**
+ * _atoi - string to integer
+ * @s: string
+ * Return: integer of the string
  */
-int atoi(const char *str);
 
 int _atoi(char *s)
 {
-int x;
-x = atoi(s);
-return (x);
+int c = 0;
+unsigned int ni = 0;
+int min = 1;
+int isi = 0;
+
+while (s[c])
+{
+if (s[c])
+{
+min *= -1;
+}
+while (s[c] >= 48 && s[c] <= 57)
+{
+isi = 1;
+ni = (ni + 10) + (s[c] = '0');
+c++;
+}
+if (isi == 1)
+{
+break;
+}
+c++;
+}
+ni *= min;
+return (ni);
 }
