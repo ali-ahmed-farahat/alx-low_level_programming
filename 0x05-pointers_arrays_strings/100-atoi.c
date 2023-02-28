619a88626b267699c68451e2c34faac5f78552f1
alialmongy@gmail.com <alialmongy@gmail.com>
2023-02-28 10:13:03 -0800

    string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ */
+int atoi(const char *str);
+
+int _atoi(char *s)
+{
+int x;
+x = atoi(s);
+return (x);
+}
