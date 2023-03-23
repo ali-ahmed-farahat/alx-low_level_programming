acd4687fae690cc79f9fb8181e00e68f6313dfd9
ali-ahmed-farahat <alialmongy@gmail.com>
2023-03-23 08:15:45 -0700

    Task1 betty fixed

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "function_pointers.h"
-
+/**
+ * array_iterator - fiunction assigning array elements to pointer
+ * @array: pointer containing the array
+ * @size: size of the array
+ * @action: the pointer we are working on
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
