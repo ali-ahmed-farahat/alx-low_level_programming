4f9b86f97b45c975e3acd39274a3fa4736a96455
ali-ahmed-farahat <70855581+ali-ahmed-farahat@users.noreply.github.com>
2023-03-25 01:39:38 +0200

    Task1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
-
 /**
  * print_numbers - print all numbers passed and seperated them with
  * the separator added as an argument
@@ -10,15 +9,12 @@
  * @n: the number of numbers that's gonna be printed
  * Return: void
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-
 	va_list ap;
 
 	va_start(ap, n);
-
 	if (separator != NULL)
 	{
 		for (i = 0; i < n - 1; i++)
@@ -33,7 +29,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			printf("%d ", va_arg(ap, int));
 		}
 	}
-
 	printf("%d\n", va_arg(ap, int));
 	va_end(ap);
 }
