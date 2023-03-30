b591300e1865afb4c0714d2aa4c01e75801393d3
ali-ahmed-farahat <70855581+ali-ahmed-farahat@users.noreply.github.com>
2023-03-31 01:16:35 +0200

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_list - function printing list + return
+ * number of it's elements
+ * @list_t: the first list element
+ * Return: the number of nodes in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+int n = 0;
+list_t * current = h;
+
+while (current != NULL)
+{
+    if (current->str == NULL)
+    printf("[0] (nil)\n");
+
+    else
+    printf("[%d] %s\n",current->len, current->str);
+
+    n++;
+    current = (current->next);
+}
+return (n);
+}
