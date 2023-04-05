72fe3b9a1dc38cda42801c48466b6654689af215
ali-ahmed-farahat <70855581+ali-ahmed-farahat@users.noreply.github.com>
2023-04-05 02:42:48 +0200

    Adding node in the beginning of list

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * *add_nodeint - adds node in the beginning of list
+ * @head: pointer to the first element of the list
+ * Return: address of the new node
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t* newnode;
+newnode = malloc(sizeof(listint_t));
+
+if (!newnode)
+return (NULL);
+
+newnode->next = *head;
+newnode->n = n;
+*head = newnode;
+
+return (newnode);
+}
