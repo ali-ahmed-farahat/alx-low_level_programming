41ed96716da6f79c42c76576d208a034210a336b
ali-ahmed-farahat <70855581+ali-ahmed-farahat@users.noreply.github.com>
2023-06-22 02:36:02 +0300

    Delete 0x09-static_libraries directory

--- 0x09-static_libraries/9-strcpy.c
+++
@@ -1,9 +0,0 @@
-#include <string.h>
-#include <stdio.h>
-#include "main.h"
-
-char *_strcpy(char *dest, char *src)
-{
-strcpy(dest, src);
-return (dest);
-}
--- 0x09-static_libraries/README.md
+++
@@ -1,1 +0,0 @@
-README file for static libraries in 0x09-static_libraries Alx's Project. 
