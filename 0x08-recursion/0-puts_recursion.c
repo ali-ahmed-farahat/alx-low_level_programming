141bacf2b3703b09b99bc752ab5695c19121e2e2
ali-ahmed-farahat <70855581+ali-ahmed-farahat@users.noreply.github.com>
2023-07-05 15:33:32 +0200

    Update to match betty standards

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,28 @@
-void _puts_recursion(char *s){
-if ((*s) == '\0')
+#include <stdio.h>
+
+/**
+ * _puts_recursion - Prints a string recursively.
+ * @s: The string to be printed.
+ */
+void _puts_recursion(char *s)
 {
-    printf("\n");
-    return;
+	if (*s == '\0')
+	{
+		printf("\n");
+		return;
+	}
+	printf("%c", *s);
+	_puts_recursion(s + 1);
 }
-printf("%c", *s);
-_puts_recursion(s + 1);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char str[] = "Anooos";
+	_puts_recursion(str);
+	return 0;
 }
