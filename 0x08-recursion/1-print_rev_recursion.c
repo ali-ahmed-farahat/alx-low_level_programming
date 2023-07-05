12c54b81ee6a60b72864d2aa45a3ae0e0bf56fd1
ali-ahmed-farahat <70855581+ali-ahmed-farahat@users.noreply.github.com>
2023-07-05 16:50:24 +0200

    Update 1-print_rev_recursion.c to match betty

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-void _print_rev_recursion(char *s){
-
-if(*s == '\0')
+/**
+ * _print_rev_recursion - Prints a string in reverse recursively
+ * @s: The string to be printed
+ */
+void _print_rev_recursion(char *s)
 {
-    return;
-}
-_print_rev_recursion(s + 1);
-putchar(*s);
+	if (*s == '\0')
+		return;
+
+	_print_rev_recursion(s + 1);
+	putchar(*s);
 }
