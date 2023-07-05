3251fe706249cd7855e9ff9af2c9561d686c3f13
ali-ahmed-farahat <70855581+ali-ahmed-farahat@users.noreply.github.com>
2023-07-05 18:25:51 +0200

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,31 +1,31 @@
+Copy code
 #include <stdio.h>
 
 /**
- * squareRoot - calculates the squareroot recursively
+ * squareRoot - calculates the square root recursively
  * @x: The base
  * @y: The guessed power
- * Return: the result square root
+ *
+ * Return: The resulting square root
  */
-
 int squareRoot(int x, int y)
 {
-    
-if (y * y < x)
-    return squareRoot(x, y + 1);
+    if (y * y < x)
+        return squareRoot(x, y + 1);
 
-if (y * y == x)
-    return (y);
-
-else
-    return (-1);
-
+    if (y * y == x)
+        return y;
+    else
+        return -1;
 }
+
 /**
- * _sqrt_recursion - calls squareRoot function to process guesses
- * @n: the number to calculate the square root to
- * Return: the result square root
+ * _sqrt_recursion - calls the squareRoot function to calculate the square root
+ * @n: The number to calculate the square root of
+ *
+ * Return: The resulting square root
  */
 int _sqrt_recursion(int n)
 {
-return squareRoot(n, 1);
+    return squareRoot(n, 1);
 }
