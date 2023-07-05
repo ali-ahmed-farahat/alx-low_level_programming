40906973461f9cf375ee8e834776a8785989ec15
ali-ahmed-farahat <70855581+ali-ahmed-farahat@users.noreply.github.com>
2023-07-05 18:55:56 +0200

    Is the number prime function (Recursively)

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * prime_check - function checking of number is prime or not recursively
+ * @n: the number to be checked
+ * @x: the number to test as a divisor
+ * Return: (0) if not prime, (1) prime
+ */
+
+int prime_check(int n, int x)
+{
+if(x == 1 || x == 0)
+return (1);
+
+if (n % x == 0)
+return (0);
+
+return (prime_check(n, x - 1));
+}
+
+/**
+ * prime_check - function checking of number is prime or not recursively
+ * @n: the number to be checked
+ * Return: (0) if not prime, (1) if prime
+ */
+
+int is_prime_number(int n)
+{
+if (n == 0 || n == 1 || n == -1)
+return (0);
+
+return (prime_check(n, n / 2));
+}
