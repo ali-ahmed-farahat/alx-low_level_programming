b753707082d602dcd8d72b8aee7b0d7529bbdb0e
ali-ahmed-farahat <alialmongy@gmail.com>
2023-07-11 19:53:00 +0200

    Fixing syntax error

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -11,7 +11,7 @@
  */
 void free_grid(int **grid, int height)
 {
-int i, j;
+int i;
 if (height <= 0)
     return;
 
@@ -27,7 +27,7 @@ while (i >= 0)
 free(grid[i]);
 i--;
 }
-free(grid)
+free(grid);
 return;
 }
 }
