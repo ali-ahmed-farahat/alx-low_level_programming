145012ee9c0ba402f811323aa726f579c50d8f0f
ali-ahmed-farahat <alialmongy@gmail.com>
2023-07-11 19:03:16 +0200

    allocating custom array using malloc

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - function allocating
+ * every element of array of custom size
+ * @width: the number of element in 1 row
+ * @height: the number of rows in the array
+ * Return: pointer to the created array (NULL on errors)
+ */
+int **alloc_grid(int width, int height)
+{
+int **array_2d;
+int i;
+int j;
+
+if (width <= 0 || height <= 0)
+    return(NULL);
+
+for (i = 0; i < height; i++)
+array_2d[i] = (int *)malloc(4 * width); /* where 4 is the size of int */
+
+if (array_2d == NULL)
+    return (NULL);
+
+for (i = 0; i < height; i++)
+{
+for (j = 0; j < width; j++)
+{
+array_2d[i][j] = (int)0;
+}
+}
+return (array_2d);
+}
