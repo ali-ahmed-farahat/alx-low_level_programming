7f92e4cb1cf94affccb1c29806d232a1e4b03dd0
ali-ahmed-farahat <alialmongy@gmail.com>
2023-07-11 19:49:55 +0200

    Freeing previosly allocated memory

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - function freeing allocated array
+ * @grid: the whole array
+ * @height: the number of rows in the array
+ * Return: void
+ */
+void free_grid(int **grid, int height);
+{
+int i, j;
+if (height <= 0)
+    return;
+
+if (grid != NULL)
+{
+for (i = height - 1; i >= 0; i--)
+{
+
+if (grid[i] != NULL)
+{
+while (i >= 0)
+{
+free(grid[i]);
+i--;
+}
+free(grid)
+return;
+}
+}
+}
+}
