c5f4478f58dfa29e53b0c80927edef3d79ad775a
ali-ahmed-farahat <alialmongy@gmail.com>
2023-07-18 02:28:15 +0200

    Editing header file#2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_H
 
 /**
- * dog - struct containing dog info
+ * struct dog - struct containing dog info
  * @name: the name of the dog (string)
  * @age: integer of age of the dog
  * @owner: name of the owner (string)
