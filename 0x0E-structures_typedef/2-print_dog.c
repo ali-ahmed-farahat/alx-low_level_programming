5c3a67117ebc8144d930183f7a9fbdb5c4180ed9
ali-ahmed-farahat <alialmongy@gmail.com>
2023-07-18 02:57:08 +0200

    Printing all struct members

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+
+/**
+ * print_dog - function printing all dog members
+ * @d: pointer to struct dog
+ * Return: Void
+ */
+
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+return;
+
+if ((d -> owner) == NULL)
+d -> owner = "(nil)";
+
+if ((d -> name) == NULL)
+d -> name = "(nil)";
+
+printf("Name: %s\n", d -> name);
+printf("Age: %f\n", d -> age);
+printf("Owner: %s", d -> owner);
+
+}
