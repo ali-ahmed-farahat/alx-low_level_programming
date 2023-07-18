4b1fa2dc0b4d9971f69b3ca9653e17f00f9796eb
ali-ahmed-farahat <alialmongy@gmail.com>
2023-07-18 04:12:37 +0200

    fixing task 4 try #1

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -35,17 +35,12 @@ free(doggie);
 return (NULL);
 }
 
-doggie->age = age;
-
-for (i = 0; name[i]; i++)
-doggie->name[i] = name[i];
-
-doggie->name[i] = '\0';
-
-for (i = 0; owner[i]; i++)
-doggie->owner[i] = owner[i];
-
-doggie->owner[i] = '\0';
-
-return (doggie);
+	for (i = 0; name[i]; i++)
+		doggie->name[i] = name[i];
+	doggie->name[i] = '\0';
+	doggie->age = age;
+	for (i = 0; owner[i]; i++)
+		doggie->owner[i] = owner[i];
+	doggie->owner[i] = '\0';
+	return (doggie);
 }
