f3789cc5dff63105d21d215dc91fbe7d4767565a
ali-ahmed-farahat <alialmongy@gmail.com>
2023-07-20 03:17:32 +0200

    matching betty style task#1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,8 +12,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 int i;
 
 if (array != NULL
-    && size != 0
-    && action != NULL)
+&& size != 0
+&& action != NULL)
 {
 for (i = 0; i < (int)size; i++)
 action(array[i]);
