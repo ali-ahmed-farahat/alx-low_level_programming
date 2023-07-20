4f7ccf461de44d8f4525c30e043fa5bba8d3de69
ali-ahmed-farahat <alialmongy@gmail.com>
2023-07-20 02:52:04 +0200

    array-iterator using ptr to function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function that iterates a specific action
+ * @array: array of integers
+ * @size: size of array
+ * @action: function that will be iterated
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+int i;
+
+if (array != NULL
+    && size != 0
+    && action != NULL)
+{
+for (i = 0; i < (int)size; i++)
+action(array[i]);
+}
+}
