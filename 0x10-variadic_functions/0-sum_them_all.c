a52bbca4673526ebfc977f7b2e5411fffc4e3e38
ali-ahmed-farahat <alialmongy@gmail.com>
2023-07-21 05:06:55 +0200

    matching betty styling

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,7 +8,8 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-int i,sum = 0;
+unsigned int i;
+int sum = 0;
 va_list args;
 
 if(n == 0)
@@ -16,7 +17,7 @@ return (0);
 
 va_start(args, n);
 
-for (i = 0; i < (int)n; i++)
+for (i = 0; i < n; i++)
 sum += va_arg(args, int);
 
 va_end(args);
