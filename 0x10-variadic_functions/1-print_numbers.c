0eb46d7199a95ea6ec762da641145894aca25539
ali-ahmed-farahat <alialmongy@gmail.com>
2023-07-21 05:49:20 +0200

    task2

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
     va_start(args, n);
 
-    for (i = 0; i < n - 1; i++)
+    for (i = 0; i < (int)n - 1; i++)
     {
         printInt(va_arg(args, int));
         fputs(separator, stdout);
