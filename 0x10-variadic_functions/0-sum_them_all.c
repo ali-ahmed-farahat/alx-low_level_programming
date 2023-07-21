c5b66875f222754327698cc1570678e7e37dc437
ali-ahmed-farahat <alialmongy@gmail.com>
2023-07-21 05:03:01 +0200

    Header file and summing function

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - vaiadic function sum all args
+ * @n: number of args
+ * Return: sum or (0) in failure
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+int i,sum = 0;
+va_list args;
+
+if(n == 0)
+return (0);
+
+va_start(args, n);
+
+for (i = 0; i < n; i++)
+sum += va_arg(args, int);
+
+va_end(args);
+
+return (sum);
+}
