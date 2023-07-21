6999ed3bf63127a1666e9d1c2ad40b92c0abec47
ali-ahmed-farahat <alialmongy@gmail.com>
2023-07-21 05:09:13 +0200

    Attempt 1

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,14 +3,15 @@
 /**
  * sum_them_all - vaiadic function sum all args
  * @n: number of args
+ *
  * Return: sum or (0) in failure
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
+va_list args;
 unsigned int i;
 int sum = 0;
-va_list args;
 
 if(n == 0)
 return (0);
