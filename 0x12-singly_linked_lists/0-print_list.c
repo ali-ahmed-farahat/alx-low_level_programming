b438795420473a9d77b52a6fcf0060ef49ad6319
ali-ahmed-farahat <alialmongy@gmail.com>
2023-07-29 01:06:00 +0200

    task 0 done

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,19 +10,16 @@
 
 size_t print_list(const list_t *h)
 {
-size_t counter = 0;
+	size_t count = 0;
 
-while (h != NULL)
-{
-
-if (h->str == NULL)
-printf("[%d] %s\n", 0, "(nil)");
-
-else
-printf("[%u] %s\n", h->len, h->str);
-
-h = h->next;
-counter++;
-}
-return (counter);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
